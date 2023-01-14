a464b07bc8c5fc12879f2c8ec9e40e50d844ed1c
Hakasraj <108168008+Hakasraj@users.noreply.github.com>
2023-01-14 21:33:10 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,24 @@
+const request = new XMLHttpRequest();
+
+request.open("GET","https://restcountries.com/v2/all");
+
+
+request.onload = function()
+{
+
+    if (request.status>=200 && request.status<300) {
+    let str = JSON.parse(this.response);
+
+for(let i = 0 ;i<str.length;i++)
+
+{
+console.log(`
+Country Name: ${str[i].name}
+Flag : ${str[i].flag}
+`)
+}
+    }
+};
+
+
+request.send();
